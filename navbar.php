e3de118ab61b7ae57eafa25a1434f0352ef2e361
Alk3bab <9019633@student.zadkine.nl>
2023-02-22 11:35:02 +0100

    css
    beetje css toegevoegd, thuis ga ik verder werken

---
+++ navbar.php
@@ -0,0 +1,51 @@
+
+
+<html>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="">nnvjnjv</a></li>
+                <li><a href="">wovnown</a></li>
+                <li><a href="">kvwnvnwken</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
+
+
+<style>
+     * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: white;
+    color: black;
+     }
+     nav {
+        background-color: var(--color-black);
+        height: 80px;
+        width: 100%;
+        display: flex;
+        justify-content: left;
+        align-items:center;
+        padding: 0 5%;
+        position: relative;
+     }
+
+     nav ul {
+        list-style: none;
+        display:flex;
+     }
+
+     nav ul li{
+        padding: 10px 30px;
+
+        position: relative;
+     }
+
+
+
+
+
+</style>
